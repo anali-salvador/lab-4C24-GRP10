684a4e16535ef74ef204a637816ebce0e518b102
Ederd Carrasco Oscco <ederd.carrasco@tecsup.edu.pe>
2026-03-24 15:15:19 -0500

    Colaborador Eder: Agregado StudentModel.java

---
+++ src/StudentModel.java
@@ -0,0 +1,8 @@
+public class StudentModel {
+    private String nombre;
+    private String codigo;
+
+    public void modelData() {
+        System.out.println("Modelo: Datos del estudiante listos para procesar.");
+    }
+}
